5821e7d62d110d277a0d1e95dd264d955d0d28d0
iryna <rinakinsha@yndex.by>
2022-06-30 22:17:52 +0300

    Solution of task 19

---
+++ Task19/Program.cs
@@ -0,0 +1,25 @@
+﻿void Palindrome(int pal)
+{
+    int revers = 0;
+    int pal1 = pal;
+
+    while (pal1 != 0)
+    {
+        revers = (revers * 10) + pal1 % 10;
+        pal1 = pal1 / 10;
+    }
+
+    if (revers == pal)
+    {
+        Console.WriteLine("Ваше число - палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Ваше число не является палиндромом");
+    }
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Palindrome(number);
